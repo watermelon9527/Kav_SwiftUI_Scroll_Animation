9b63110036d25d159d2c566529ce2d8797da96dc
Neil Chan <neil.chan@card.rakuten.com.tw>
2025-05-07 10:24:00 +0800

    Initial Commit

---
+++ Kav_SwiftUI_Scroll_Animation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kav_SwiftUI_Scroll_Animation
+//
+//  Created by Neil Chan on 2025/5/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kav_SwiftUI_Scroll_Animation/Kav_SwiftUI_Scroll_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kav_SwiftUI_Scroll_AnimationApp.swift
+//  Kav_SwiftUI_Scroll_Animation
+//
+//  Created by Neil Chan on 2025/5/7.
+//
+
+import SwiftUI
+
+@main
+struct Kav_SwiftUI_Scroll_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
